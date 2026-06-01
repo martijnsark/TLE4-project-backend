6bab7e5ca2cb9aa7595c99fa965844eb46e3100c
neotroenopawiro <302869724@student.rocmondriaan.nl>
2026-06-01 15:27:16 +0200

    login completed

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
