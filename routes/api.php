6a774be13a118d1c3da4fbc6bffb9ac723985ad7
1073500 <1073500@hr.nl>
2026-06-02 14:43:48 +0200

    middleware

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\ArticleController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AdminMiddleware;
 
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -11,17 +12,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-//articles
-
-
 Route::get('articles', [ArticleController::class, 'index']);
 Route::get('articles/{article}', [ArticleController::class, 'show']);
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::middleware('admin')->group(function () {
-        Route::post('articles', [ArticleController::class, 'store']);
-        Route::put('articles/{article}', [ArticleController::class, 'update']);
-        Route::patch('articles/{article}', [ArticleController::class, 'update']);
-        Route::delete('articles/{article}', [ArticleController::class, 'destroy']);
-        Route::get('articles/{article}/edit', [ArticleController::class, 'edit']);
-    });
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::post('articles', [ArticleController::class, 'store']);
+    Route::put('articles/{article}', [ArticleController::class, 'update']);
+    Route::patch('articles/{article}', [ArticleController::class, 'update']);
+    Route::delete('articles/{article}', [ArticleController::class, 'destroy']);
+    Route::get('articles/{article}/edit', [ArticleController::class, 'edit']);
 });
